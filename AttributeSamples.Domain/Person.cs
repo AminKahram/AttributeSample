5dde8e497d2a5e2bca5fcbb782d2c681b0ef39d3
Amin <KahramAmin@gmail.com>
2023-09-27 11:06:07 +0330

    Data validation and serialization

--- AttributeSamples.Domain/Person.cs
+++ AttributeSamples.Domain/Person.cs
@@ -1,15 +1,20 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 
 namespace AttributeSamples.Domain
 {
     [DebuggerDisplay("Person Name is {FirstName} and Age is {Age}")]
     [DebuggerTypeProxy(typeof(PersonDebuggerTypeProxy ))]
+    [Serializable]
     public class Person
     {
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        [Required]
+        [StringLength(100)]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
-
+        [NonSerialized]
+        private int Count { get; set; }
     }
 }
