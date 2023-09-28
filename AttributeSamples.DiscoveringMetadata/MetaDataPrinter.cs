b1fbc3525a3d8322920806bf6ca237d508e64359
Amin <KahramAmin@gmail.com>
2023-09-28 07:56:46 +0330

    Reflection MetaData

---
+++ AttributeSamples.DiscoveringMetadata/MetaDataPrinter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeSamples.DiscoveringMetadata;
+
+public class MetaDataPrinter
+{
+    private readonly Type type;
+
+    public MetaDataPrinter(Type type)
+    {
+        this.type = type;
+    }
+    public void Print()
+    {
+        PrintMainInfo(); 
+        PrintMethodInfo();
+        PrintPropertyInfo();
+        printFieldInfo();
+    }
+
+    private void printFieldInfo()
+    {
+        Console.WriteLine($"***************** Field Information of type {type.Name} *******************");
+        var feildInfos = type.GetFields();
+        foreach (var feildInfo in feildInfos)
+        {
+            Console.WriteLine($"{feildInfo.Name}, {feildInfo.FieldType}");
+        }
+    }
+
+    private void PrintPropertyInfo()
+    {
+        Console.WriteLine($"***************** Properties Information of type {type.Name} *******************");
+        var propertyInfos = type.GetProperties();
+        foreach ( var propertyInfo in propertyInfos )
+        {
+            Console.WriteLine($"{propertyInfo.Name}, {propertyInfo.PropertyType}");
+        }
+
+    }
+
+    private void PrintMethodInfo()
+    {
+        Console.WriteLine($"***************** Method Information of type {type.Name} *******************");
+        var methodInfo = type.GetMethods();
+        foreach ( var method in methodInfo )
+        {
+            Console.Write($"{method.Name} (");
+            var parameters = method.GetParameters();
+            foreach ( var param in parameters )
+            {
+                Console.Write($"{param.ParameterType}, {param.Name}");
+            }
+            Console.WriteLine(")");
+        }
+
+    }
+
+    private void PrintMainInfo()
+    {
+        Console.WriteLine($"***************** Information of type {type.Name} *******************");
+        Console.WriteLine($"FullName: {type.FullName}");
+        Console.WriteLine($"AssemblyQualifiedName :{type.AssemblyQualifiedName}");
+        Console.WriteLine($"IsAbstract :{type.IsAbstract}");
+    }
+}
---
+++ AttributeSamples.DiscoveringMetadata/Program.cs
@@ -0,0 +1,14 @@
+﻿using AttributeSamples.DiscoveringMetadata;
+using AttributeSamples.Domain;
+
+var intPrinter = new MetaDataPrinter(typeof(int));
+var personPrinter = new MetaDataPrinter(typeof(Person));
+
+intPrinter.Print();
+Console.WriteLine("Press a key to continue");
+Console.ReadLine();
+Console.Clear();
+
+personPrinter.Print();
+Console.WriteLine("Press a key to exit");
+Console.ReadKey();
