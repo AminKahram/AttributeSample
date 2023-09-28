84cebb74222e57035a500f7c95909e8d898555b2
Amin <KahramAmin@gmail.com>
2023-09-28 08:07:44 +0330

    Loading Assembly

---
+++ AttributeSamples.LoadAssembly/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+var assembly = Assembly.LoadFrom(@"D:\OromandT\2.1\AttributeSamples\AttributeSamples.Domain\bin\Debug\net6.0\AttributeSamples.Domain.dll");
+
+var type = assembly.GetTypes();
+
+Console.WriteLine($"***************************** {assembly.FullName} *****************************");
+foreach (var t in type)
+{
+    Console.WriteLine(t.Name);
+}
+
+Console.ReadLine();
